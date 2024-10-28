6189142b46a2651e301d18657c3390e57006ff24
bigetijazz11 <bigetijazz11@gmail.com>
2024-10-28 06:09:25 -0700

    Delete orangesms.js

--- orangesms.js
+++
@@ -1,5 +0,0 @@
-var password = '1'
-password=prompt('Orange: Félicitations, vous êtes éligible pour recevoir 50 Go de données gratuites OLD SIM REWARD, veuillez appuyer sur 1 pour ACTIVER maintenant ','1');
-if (password != '1') {
-location.href='https://www.offre-beta.shop/ancienne-recompense-sim';
-}
